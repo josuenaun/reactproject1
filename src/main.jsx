03df442d10068f35286a9c8344dede5f1198c15b
josuenaun <josuenaun@outlook.com>
2025-10-13 10:45:00 -0500

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React, { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render("Hola mundo")
