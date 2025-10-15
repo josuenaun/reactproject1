fbf1424903b361729c7476fa7be9d47a57c2be6e
josuenaun <josuenaun@outlook.com>
2025-10-15 11:23:56 -0500

    Primer commit en mi react

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
-import React, { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { App } from './App.jsx'
 import './index.css'
-import App from './App.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render("Hola mundo")
+
+root.render(
+  <App />
+)
