3778868480e75d69c7885308593f9148f0d1bc12
josuenaun <josuenaun@outlook.com>
2025-10-16 17:25:08 -0500

    Finalización de curso Midudev TwitterFollowCard.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
+const users = [
+  {
+    userName: 'midudev',
+    name: 'Miguel Ángel Durán',
+    isFollowing: true
+  },
+  {
+    userName: 'pheralb',
+    name: 'Pablo H.',
+    isFollowing: false
+  },
+  {
+    userName: 'PacoHdezs',
+    name: 'Paco Hdez',
+    isFollowing: true
+  },
+  {
+    userName: 'TMChein',
+    name: 'Tomas',
+    isFollowing: false
+  }
+]
+
 export function App () {
-    return (
-        <section className="App">
-            <TwitterFollowCard userName="midudev" name="Miguel Angel Durán"/>
-            <TwitterFollowCard userName="pheralb" name="Pablo Hernandez"/>
-            <TwitterFollowCard userName="elonmusk" name="Elon Musk"/>
-            <TwitterFollowCard userName="vxnder" name="Vanderhart"/>
-        </section>
-    )
+  return (
+    <section className='App'>
+      {
+        users.map(({ userName, name, isFollowing }) => (
+          <TwitterFollowCard
+            key={userName}
+            userName={userName}
+            initialIsFollowing={isFollowing}
+          >
+            {name}
+          </TwitterFollowCard>
+        ))
+      }
+    </section>
+  )
 }
