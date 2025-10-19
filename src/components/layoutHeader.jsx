b6f58fe254854514adafecce9eeba4e626c80d68
josuenaun <josuenaun@outlook.com>
2025-10-19 09:09:33 -0500

    Header completo

--- src/components/layoutHeader.jsx
+++ src/components/layoutHeader.jsx
@@ -1,12 +1,80 @@
-import React from 'react'
+import React from "react";
 
-export const LayoutHeader = ({username,img}) => {
+export const LayoutHeader = ({ username, img }) => {
   return (
-    <div className='flex w-full bg-amber-800'>
-        <div-left>
-            <h1>{username}</h1>
-            <img src={`https://unavatar.io/${img}`}/>
-        </div-left>
+    <div className="flex w-full bg-header text-text-base  justify-between px-6 py-3">
+      <div className="flex items-center gap-3">
+        <h1 className="font-bold">{username}</h1>
+        <img
+          src={`https://unavatar.io/${img}`}
+          className=" w-auto h-10 rounded-full"
+        />
+      </div>
+      <nav className="flex items-center gap-8 ">
+        <a href="#" className="flex gap-3 hover:text-amber-500 transition-colors">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="icon icon-tabler icons-tabler-outline icon-tabler-library-photo"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M7 3m0 2.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667z" />
+            <path d="M4.012 7.26a2.005 2.005 0 0 0 -1.012 1.737v10c0 1.1 .9 2 2 2h10c.75 0 1.158 -.385 1.5 -1" />
+            <path d="M17 7h.01" />
+            <path d="M7 13l3.644 -3.644a1.21 1.21 0 0 1 1.712 0l3.644 3.644" />
+            <path d="M15 12l1.644 -1.644a1.21 1.21 0 0 1 1.712 0l2.644 2.644" />
+          </svg>
+          Galería
+        </a>
+        <a href="#" className="flex gap-3 hover:text-amber-500 transition-colors">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            class="icon icon-tabler icons-tabler-filled icon-tabler-file-cv"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M12 2l.117 .007a1 1 0 0 1 .876 .876l.007 .117v4l.005 .15a2 2 0 0 0 1.838 1.844l.157 .006h4l.117 .007a1 1 0 0 1 .876 .876l.007 .117v9a3 3 0 0 1 -2.824 2.995l-.176 .005h-10a3 3 0 0 1 -2.995 -2.824l-.005 -.176v-14a3 3 0 0 1 2.824 -2.995l.176 -.005zm-2.5 8a2.5 2.5 0 0 0 -2.5 2.5v3a2.5 2.5 0 1 0 5 0a1 1 0 0 0 -2 0a.5 .5 0 1 1 -1 0v-3a.5 .5 0 1 1 1 0a1 1 0 0 0 2 0a2.5 2.5 0 0 0 -2.5 -2.5m6.743 .03a1 1 0 0 0 -1.213 .727l-.53 2.119l-.53 -2.119a1 1 0 1 0 -1.94 .486l1.5 6c.252 1.01 1.688 1.01 1.94 0l1.5 -6a1 1 0 0 0 -.727 -1.213m-1.244 -7.031l4.001 4.001h-4z" />
+          </svg>
+          Portfolio
+        </a>
+        <a href="#" className="flex gap-3 hover:text-amber-500 transition-colors">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="icon icon-tabler icons-tabler-outline icon-tabler-user"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+            <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+          </svg>
+          Contact Me
+        </a>
+      </nav>
+      <div className=" flex items-center">
+        <div>
+          <img
+            src={`https://unavatar.io/${img}`}
+            className=" w-auto h-10 rounded-full"
+          />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
